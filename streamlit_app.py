d59895ebc6c4910e9627bf9036a038de6d85a651
ASHOKKURUBA89 <ashok.kuruba89@gmail.com>
2025-03-27 21:17:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingredeient_list:
     if time_to_insert:
        session.sql(my_insert_stmt).collect()
         
-       st.success('Your Smoothie is ordered!', icon=":white_check_mark:")
+       st.success('Your Smoothie is ordered!', icon='✅')
